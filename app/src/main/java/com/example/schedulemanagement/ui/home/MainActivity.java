338cd900d745c08416a7279ef402af8b0d607af4
Vunhan1002 <vunhan10022004@gmail.com>
2025-08-12 07:49:20 +0700

    Hoàn thành chức năng đăng nhập

--- app/src/main/java/com/example/schedulemanagement/ui/home/MainActivity.java
+++ app/src/main/java/com/example/schedulemanagement/ui/home/MainActivity.java
@@ -1,26 +1,32 @@
 package com.example.schedulemanagement.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 import com.example.schedulemanagement.R;
+import com.example.schedulemanagement.ui.notification.NotificationsActivity;
+import com.example.schedulemanagement.ui.progress.ProgressActivity;
+import com.example.schedulemanagement.ui.schedule.ScheduleActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.activity_home);
+
+        findViewById(R.id.rowSchedule).setOnClickListener( v ->
+                startActivity(new Intent(this, ScheduleActivity.class)));
+
+        findViewById(R.id.rowContent).setOnClickListener( v ->{
+            // TODO: sau này mở nội dung bài giảng
+                });
+
+        findViewById(R.id.rowProgress).setOnClickListener( v ->
+                startActivity(new Intent(this, ProgressActivity.class)));
+
+        findViewById(R.id.rowNotifications).setOnClickListener( v ->
+                startActivity(new Intent(this, NotificationsActivity.class)));
+
     }
 }
