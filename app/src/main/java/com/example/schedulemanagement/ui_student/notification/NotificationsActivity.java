9157a57d88de83f76e7ae36e9e0976888ea05edf
Vunhan1002 <vunhan10022004@gmail.com>
2025-08-21 21:21:16 +0700

    Hoàn thành chức năng đăng nhập cho admin

--- app/src/main/java/com/example/schedulemanagement/ui_student/notification/NotificationsActivity.java
+++ app/src/main/java/com/example/schedulemanagement/ui_student/notification/NotificationsActivity.java
@@ -1,16 +1,17 @@
-package com.example.schedulemanagement.ui.schedule;
+package com.example.schedulemanagement.ui_student.notification;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.schedulemanagement.R;
 
-public class ScheduleActivity extends AppCompatActivity {
+public class NotificationsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_schedule);
+        setContentView(R.layout.activity_notifications);
         findViewById(R.id.btnBack).setOnClickListener(v -> finish());
-        // TODO: sẽ thay static UI bằng RecyclerView + data thật
+        // TODO: sẽ thay static include bằng RecyclerView + data thật
+
     }
 }
--- app/src/main/java/com/example/schedulemanagement/ui_student/progress/ProgressActivity.java
+++ app/src/main/java/com/example/schedulemanagement/ui_student/progress/ProgressActivity.java
@@ -1,17 +1,16 @@
-package com.example.schedulemanagement.ui.notification;
+package com.example.schedulemanagement.ui_student.progress;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.schedulemanagement.R;
 
-public class NotificationsActivity extends AppCompatActivity {
+public class ProgressActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notifications);
+        setContentView(R.layout.activity_progress);
         findViewById(R.id.btnBack).setOnClickListener(v -> finish());
-        // TODO: sẽ thay static include bằng RecyclerView + data thật
-
+        // TODO: sau sẽ render % động
     }
 }
